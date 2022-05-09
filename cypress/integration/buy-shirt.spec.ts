f1e747613a44ff6f2bc653adf7fc2dcb2d195afc
Jennifer Castiblanco <82177981+JenniferCastiblanco@users.noreply.github.com>
2022-05-09 15:29:07 -0500

    Setups eslintrc

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -1,18 +1,18 @@
 describe("Buy a t-shirt", () => {
   it("then the t-shirt should be bought", () => {
-    cy.visit('http://automationpractice.com/');
-    cy.get('.sf-menu > :nth-child(3) > a').click();
-    cy.get('.ajax_add_to_cart_button > span').click();
-    cy.get('.button-container > .button-medium > span').click();
-    cy.get('.cart_navigation > .button > span').click();
-    cy.get('#email').type('aperdomobo@gmail.com');
-    cy.get('#passwd').type('WorkshopProtractor');
-    cy.get('#SubmitLogin > span').click();
-    cy.get('.cart_navigation > .button > span').click();
-    cy.get('#cgv').click();
-    cy.get('.cart_navigation > .button > span').click();
-    cy.get('.bankwire').click();
-    cy.get('#cart_navigation > .button > span').click();
-    cy.get('.cheque-indent > .dark').should('have.text','Your order on My Store is complete.');
+    cy.visit("http://automationpractice.com/");
+    cy.get(".sf-menu > :nth-child(3) > a").click();
+    cy.get(".ajax_add_to_cart_button > span").click();
+    cy.get(".button-container > .button-medium > span").click();
+    cy.get(".cart_navigation > .button > span").click();
+    cy.get("#email").type("aperdomobo@gmail.com");
+    cy.get("#passwd").type("WorkshopProtractor");
+    cy.get("#SubmitLogin > span").click();
+    cy.get(".cart_navigation > .button > span").click();
+    cy.get("#cgv").click();
+    cy.get(".cart_navigation > .button > span").click();
+    cy.get(".bankwire").click();
+    cy.get("#cart_navigation > .button > span").click();
+    cy.get(".cheque-indent > .dark").should("have.text", "Your order on My Store is complete.");
   });
 });
